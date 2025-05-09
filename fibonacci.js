9ae2b64f341d66cb6a84350ceb3803722fda68b8
Valerian Ha <ha.valerian@gmail.com>
2025-05-08 21:56:47 -0400

    Complete fibonacci recursion.

---
+++ fibonacci.js
@@ -0,0 +1,28 @@
+const fibs = (num) => {
+  const sequence = [];
+  for (let i = 0; i < num; i++) {
+    if (i === 0) {
+      sequence.push(0);
+    } else if (i === 1) {
+      sequence.push(1);
+    } else {
+      sequence.push(sequence[i - 2] + sequence[i - 1]);
+    }
+  }
+  return sequence;
+};
+
+const fibsRec = (num) => {
+  if (num === 1) {
+    return [0];
+  } else if (num === 2) {
+    return [0, 1];
+  } else {
+    const seq = fibsRec(num - 1);
+    seq.push(seq[seq.length - 1] + seq[seq.length - 2]);
+    return seq;
+  }
+};
+
+console.log(fibs(2));
+console.log(fibsRec(2));
